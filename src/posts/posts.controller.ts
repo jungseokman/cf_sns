cda05eccf2c41b391660f1cc8190e5ed929ca12f
jungseokman <seokman.jung@gmail.com>
2025-01-07 16:49:10 +0900

    modify

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,45 +3,24 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
-import { PostsService } from './posts.service';
-
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'test',
-    title: 'My second Post',
-    content: 'This is my second post',
-    likeCount: 200000,
-    commentCount: 130000,
-  },
-];
+import { PostModel, PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  getPosts(): PostModel[] {
-    return posts;
+  getPosts() {
+    return this.postsService.getAllPosts();
   }
 
   @Get(':id')
   getPost(@Param('id') id: string): PostModel {
-    return posts.find((post) => post.id === parseInt(id));
+    return this.postsService.getPostById(parseInt(id));
   }
 
   @Post()
@@ -50,16 +29,7 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string,
   ): PostModel {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
-      author,
-      title,
-      content,
-      likeCount: 0,
-      commentCount: 0,
-    };
-    posts.push(post);
-    return post;
+    return this.postsService.createPost(author, title, content);
   }
 
   @Put(':id')
@@ -69,36 +39,11 @@ export class PostsController {
     @Body('content') content?: string,
     @Body('author') author?: string,
   ): PostModel {
-    const post = posts.find((post) => post.id === parseInt(id));
-    if (!post) {
-      throw new NotFoundException('Post not found');
-    }
-    console.log(`1 => ${post.content}`);
-
-    if (title) post.title = title;
-    console.log(`2 => ${post.content}`);
-    if (content) post.content = content;
-    console.log(`3 => ${post.content}`);
-    if (author) post.author = author;
-    console.log(`4 => ${post.content}`);
-
-    posts.map((prevPost) => {
-      if (prevPost.id === parseInt(id)) {
-        return post;
-      }
-      return prevPost;
-    });
-
-    return post;
+    return this.postsService.updatePost(parseInt(id), author, title, content);
   }
 
   @Delete(':id')
   deletePost(@Param('id') id: string): PostModel {
-    const post = posts.find((post) => post.id === parseInt(id));
-    if (!post) {
-      throw new NotFoundException('Post not found');
-    }
-    posts = posts.filter((post) => post.id !== parseInt(id));
-    return post;
+    return this.postsService.deletePost(parseInt(id));
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,70 @@
 import { Injectable } from '@nestjs/common';
 
+export interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'test',
+    title: 'My second Post',
+    content: 'This is my second post',
+    likeCount: 200000,
+    commentCount: 130000,
+  },
+];
+
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts(): PostModel[] {
+    return posts;
+  }
+
+  getPostById(id: number): PostModel {
+    return posts.find((post) => post.id === id);
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const post: PostModel = {
+      id: posts.length + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+    posts.push(post);
+    return post;
+  }
+
+  updatePost(
+    id: number,
+    author: string,
+    title: string,
+    content: string,
+  ): PostModel {
+    const index = posts.findIndex((post) => post.id === id);
+    posts[index] = {
+      id,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+    return posts[index];
+  }
+
+  deletePost(id: number): PostModel {
+    const index = posts.findIndex((post) => post.id === id);
+    const post = posts[index];
+    posts.splice(index, 1);
+    return post;
+  }
+}
